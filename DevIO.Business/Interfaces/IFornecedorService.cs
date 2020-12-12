58252f9ecaa7724b4b4001e03f9a499bec53d3ea
josenerydev <josenerydev@gmail.com>
2020-12-12 11:52:52 -0300

    Add business layer

---
+++ DevIO.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Business.Models;
+
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorService
+    {
+        Task<bool> Adicionar(Fornecedor fornecedor);
+        Task<bool> Atualizar(Fornecedor fornecedor);
+        Task<bool> Remover(Guid id);
+        Task AtualizarEndereco(Endereco endereco);
+    }
+}
